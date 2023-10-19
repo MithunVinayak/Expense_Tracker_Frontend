b8649856ddd89476af9cf0a98c480b9a5342dd56
MithunVinayak <mithunvinayak10@gmail.com>
2023-10-19 19:05:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useMemo}  from "react";
+import styled from "styled-components";
+import bg from './img/bg.png'
+import {MainLayout} from './styles/LayoutStyle'
+import Orb from './components/Orb/orb'
+import Navigation from './components/Navigation/Navigation'
+import Incomes from "./components/Incomes/Incomes";
+import Expenses from './components/Expenses/Expenses';
+import { useGlobalContext } from "./context/GlobalContext";
+import Dashboard from "./components/Dashboard/Dashboard";
+
 
 function App() {
+  const global = useGlobalContext()
+  console.log(global)
+  const [active, setActive] = React.useState(1);
+  const orbMemo = useMemo(() => {
+    return <Orb />
+  },[])
+ 
+  const displayData = () => {
+    switch(active){
+      case 1:
+        return <Dashboard />
+      case 2:
+        return <Dashboard />
+      case 3:
+        return <Incomes />
+      case 4: 
+        return <Expenses />
+      default: 
+        return <Dashboard />
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppStyled bg = {bg} className="App">
+      {orbMemo}
+      <MainLayout>
+      <Navigation active={active} setActive={setActive} />
+      <main>
+        {displayData()}
+      </main>
+      </MainLayout>
+    </AppStyled>
   );
 }
+
+const AppStyled = styled.div `
+height: 100vh;
+background-image: url(${props => props.bg});
+position: relative;
+main{
+  flex: 1;
+  background: rgba(252, 246, 249, 0.78);
+  border: 3px solid #FFFFFF;
+  backdrop-filter: blur(4.5px);
+  border-radius: 30px;
+  overflow-x: hidden;
+  &::-webkit-scrollbar{
+    width: 0;
+  }
+}
+`
 
 export default App;
---
+++ src/context/GlobalContext.js
@@ -0,0 +1,98 @@
+import React,{useState,useContext} from "react";
+import axios from "axios";
+
+const GlobalContext = React.createContext()
+
+const BASE_URL = "https://expense-tracker-r2w4.onrender.com/api/v1/";
+
+export const GlobalProvider = ({children}) => {
+    const [incomes, setIncomes] = useState([])
+    const [expenses, setExpenses] = useState([])
+    const [error, setError] = useState(null)
+
+    const addIncome = async (income) => {
+        const response = await axios.post(`${BASE_URL}add-income`, income)
+            .catch((err) =>{
+                setError(err.response.data.message)
+            })
+            getIncomes()
+    }
+    const getIncomes = async () => {
+        const response = await axios.get(`${BASE_URL}get-incomes`)
+        setIncomes(response.data)
+        console.log(response.data)
+    }
+    const deleteIncome = async (id) => {
+        const res  = await axios.delete(`${BASE_URL}delete-income/${id}`)
+        getIncomes()
+    }
+    const totalIncome = () => {
+        let totalIncome = 0;
+        incomes.forEach((income) =>{
+            totalIncome = totalIncome + income.amount
+        })
+
+        return totalIncome;
+    }
+
+    const addExpense = async (expense) => {
+        const response = await axios.post(`${BASE_URL}add-expense`, expense)
+            .catch((err) =>{
+                setError(err.response.data.message)
+            })
+            getExpenses()
+    }
+    const getExpenses = async () => {
+        const response = await axios.get(`${BASE_URL}get-expenses`)
+        setExpenses(response.data)
+        console.log(response.data)
+    }
+    const deleteExpenses = async (id) => {
+        const res  = await axios.delete(`${BASE_URL}delete-expense/${id}`)
+        getExpenses()
+    }
+    const totalExpenses = () => {
+        let totalExpense = 0;
+        expenses.forEach((expense) =>{
+            totalExpense = totalExpense + expense.amount
+        })
+
+        return totalExpense;
+    }
+    const totalBalance = () => {
+        return totalIncome() - totalExpenses()
+    }
+
+    const transactionHistory = () => {
+        const history = [...incomes, ...expenses]
+        history.sort((a, b) => {
+            return new Date(b.createdAt) - new Date(a.createdAt)
+        })
+
+        return history.slice(0, 3)
+    }
+    return (
+        <GlobalContext.Provider value={{
+            addIncome,
+            getIncomes,
+            incomes,
+            deleteIncome,
+            totalIncome,
+            addExpense,
+            getExpenses,
+            expenses,
+            deleteExpenses,
+            totalExpenses,
+            totalBalance,
+            transactionHistory,
+            error,
+            setError
+            }}>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
+
+export const useGlobalContext = () =>{
+    return useContext(GlobalContext)
+}
